653595b644cd1a118d9aaf692cbb4836db0e548b
YEVHEN_ZAKHARCH <Y.Zakharchenko@steel.mil.ua>
2024-05-01 17:02:47 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TheHeader from "@/components/TheHeader.vue";
+import EntryEditor from "./components/EntryEditor.vue";
+import EntryCard from "@/components/EntryCard.vue";
+import { reactive } from 'vue'
+const user = reactive(
+  {
+    id: 1,
+    username: 'Eugene'
+  }
+)
+</script>
+
+<template>
+  <main class="container m-auto p-10">
+    <TheHeader />
+    <EntryEditor />
+    <ul>
+      <li>
+        <EntryCard />
+      </li>
+    </ul>
+  </main>
+</template>
